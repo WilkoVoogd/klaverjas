b454926bdd76b977010afa64c258c4a823716206
WilkoVoogd <40392814+WilkoVoogd@users.noreply.github.com>
2020-07-26 16:46:21 +0200

    Test for CardDealer

---
+++ test/java/Boom/CardDealerTest.java
@@ -0,0 +1,36 @@
+package Boom;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardDealerTest {
+
+    Player a;
+    Player b;
+    Player c;
+    Player d;
+    List<Player> players;
+
+    @BeforeEach
+    void setUp() {
+        a = new Player("A");
+        b = new Player("B");
+        c = new Player("C");
+        d = new Player("D");
+        players = Arrays.asList(a, b, c, d);
+    }
+
+    @Test
+    void testDealHands() {
+        CardDealer dealer = new CardDealer();
+        dealer.dealHands(players);
+        for (Player player : players) {
+            assertEquals(8, player.getHand().getCards().size());
+        }
+    }
+}
